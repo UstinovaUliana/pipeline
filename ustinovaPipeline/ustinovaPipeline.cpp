b4777523eaa2878c913de7af23f8bdf65217cc58
UstinovaUliana <mirrorrune21363@gmail.com>
2021-09-14 15:28:59 +0300

    добавлена станция (такое себе пока что)

--- ustinovaPipeline/ustinovaPipeline.cpp
+++ ustinovaPipeline/ustinovaPipeline.cpp
@@ -7,26 +7,51 @@ using namespace std;
 struct Pipe {
     int id;
     int d;
-
+    bool sost;
 };
 
+struct Stantia {
+    int l;
+    bool rab;
+};
 void coutPipe(Pipe& truba)
 {
-    cout << "Id: " << truba.id << endl << "Диаметр: " << truba.d << endl;
+    cout << "Id: " << truba.id << endl << "Диаметр: " << truba.d << endl << "Rabotaet: " << truba.sost << endl;
 }
 
 Pipe createPipe() {
     Pipe truba;
     truba.id = 0;
+    truba.sost = true;
     cout << "Введите диаметр:\n";
     cin >> truba.d;
     return truba;
 }
 
+Stantia createStantia() {
+    Stantia stan;
+    cout << "Vvedite dlinu:\n";
+    cin >> stan.l;
+    stan.rab = true;
+    return stan;
+}
+
+void coutStantia(Stantia& stan);
+
 int main()
 {
+
     Pipe truba = createPipe();
     coutPipe(truba);
+    Stantia stan = createStantia();
+    
+    coutStantia(stan);
+
+}
+
+void coutStantia(Stantia& stan)
+{
+    cout << "Dlina: " << stan.l << endl << "Rabotaet: " << stan.rab << endl;
 }
 
 
