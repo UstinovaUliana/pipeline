f62f9c66bae0e82b76d1c3bc0de4e161d0e35c91
Uliana <mirrorrune21363@gmail.com>
2021-09-20 21:14:26 +0300

    Добавлен русский язык, изменены параметры труб и станций, добавлено меню

--- ustinovaPipeline/ustinovaPipeline.cpp
+++ ustinovaPipeline/ustinovaPipeline.cpp
@@ -2,56 +2,77 @@
 //
 
 #include <iostream>
+#include <locale.h>
 using namespace std;
 
 struct Pipe {
     int id;
     int d;
-    bool sost;
+    int l;
+    bool rem;
 };
 
 struct Stantia {
-    int l;
-    bool rab;
+    int id;
+    string name;
+    int ceh;
+    int cehRab;
+    int eff;
 };
 void coutPipe(Pipe& truba)
 {
-    cout << "Id: " << truba.id << endl << "Диаметр: " << truba.d << endl << "Rabotaet: " << truba.sost << endl;
+    cout << "Id: " << truba.id << endl << "Диаметр: " << truba.d << endl << "Длина: " << truba.l << endl << "В ремонте: " << truba.rem << endl;
+}
+void coutStantia(Stantia& stan)
+{
+    cout << "Id " << stan.id << endl << "Название: " << truba.name << endl << "Кол-во цехов " << truba.ceh << endl << "Цехов в работе: " << truba.cehRab << endl << "Эффективность: " << truba.eff << endl;
 }
 
 Pipe createPipe() {
     Pipe truba;
     truba.id = 0;
-    truba.sost = true;
-    cout << "Введите диаметр:\n";
+    truba.rem = false;
+    cout << "Введите диаметр трубы:\n";
     cin >> truba.d;
+    cout << "Введите длину трубы:\n";
+    cin >> truba.l;
     return truba;
 }
 
 Stantia createStantia() {
     Stantia stan;
-    cout << "Vvedite dlinu:\n";
-    cin >> stan.l;
-    stan.rab = true;
+    stan.id = 0;
+    cout << "Введите название станции:\n";
+    cin >> stan.name;
+    stan.ceh = 30;
+    stan.cehRab = 30;
+    stan.eff = 100;
     return stan;
 }
 
-void coutStantia(Stantia& stan);
 
 int main()
 {
-
-    Pipe truba = createPipe();
-    coutPipe(truba);
-    Stantia stan = createStantia();
-    
-    coutStantia(stan);
+    setlocale(LC_ALL, "Russian");
+    cout << " 1. Добавить трубу \n 2. Добавить КС \n 3. Просмотр всех объектов \n 4. Редактировать трубу \n 5. Редактировать КС \n 6. Сохранить \n 7. Загрузить \n 0. Выход \n";
+    int a;
+    cin >> a;
+    if (a == 1) {
+        Pipe truba = createPipe();
+        coutPipe(truba);
+    }
+    if (a == 2) {
+        Stantia stan = createStantia();
+        coutStantia(stan);
+    }
+   // if (a == 4) {
+    //truba.rem = true;
+    //coutPipe(truba);
+}
+   
 
 }
 
-void coutStantia(Stantia& stan)
-{
-    cout << "Dlina: " << stan.l << endl << "Rabotaet: " << stan.rab << endl;
-}
+
 
 
