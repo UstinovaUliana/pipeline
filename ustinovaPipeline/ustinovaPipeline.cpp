26f43c131dc56a05d8a98f80aaae88d50e9443d9
Uliana <mirrorrune21363@gmail.com>
2021-10-03 18:01:58 +0300

    Ввод/ вывод и проверка корректности

--- ustinovaPipeline/ustinovaPipeline.cpp
+++ ustinovaPipeline/ustinovaPipeline.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <locale.h>
 #include <string>
+#include <fstream>
 using namespace std;
 
 struct Pipe {
@@ -24,22 +25,67 @@ Pipe createPipe() {
     Pipe truba;
     truba.id = 1;
     truba.rem = false;
-    cout << "Введите диаметр трубы, мм:";
-    cin >> truba.d;
-    cout << "Введите длину трубы, км:";
-    cin >> truba.l;
+    do {
+        cin.clear();
+        cin.ignore(2000, '\n');
+        cout << "Введите диаметр трубы, мм:";
+        cin >> truba.d;
+    } while (cin.fail() || truba.d < 50 || truba.d>2000);
+     do {
+         cin.clear();
+         cin.ignore(2000, '\n');
+        cout << "Введите длину трубы, км:";
+        cin >> truba.l;
+     } while (cin.fail() || truba.l < 0.05 || truba.l>100);
+    
     return truba;
 }
 
 Stantia createStantia() {
     Stantia stan;
     stan.id = 2;
-    cout << "Введите название станции (на английском):";
-    cin >> stan.name;
-    cout << "Введите количество цехов:";
-    cin >> stan.ceh;
+    do {
+        cout << "Введите название станции (на английском):";
+        cin >> stan.name;
+    } while (cin.fail());
+    
+    do {
+        cin.clear();
+        cin.ignore(2000, '\n');
+        cout << "Введите количество цехов:";
+        cin >> stan.ceh;
+    } while (cin.fail() || stan.ceh < 1 || stan.ceh>10);
     stan.cehRab = stan.ceh;
     stan.eff = 100;
+    return stan;
+}
+
+Pipe loadPipe() {
+    Pipe truba;
+    ifstream fin;
+    fin.open("InTruba.txt", ios::in);
+    if (fin.is_open()) {
+        fin >> truba.id;
+        fin >> truba.d;
+        fin >> truba.l;
+        fin >> truba.rem;
+        fin.close();
+    }
+    return truba;
+}
+
+Stantia loadStantia() {
+    Stantia stan;
+    ifstream fin;
+    fin.open("InStantia.txt", ios::in);
+    if (fin.is_open()) {
+        fin >> stan.id;
+        fin >> stan.name;
+        fin >> stan.ceh;
+        fin >> stan.cehRab;
+        fin >> stan.eff;
+        fin.close();
+    }
     return stan;
 }
 
@@ -54,6 +100,31 @@ void coutStantia(Stantia& stan)
 {
     cout << "Станция" << endl << "Id: " << stan.id << endl << "Название: " << stan.name << endl << "Кол-во цехов " << stan.ceh << endl << "Цехов в работе: " << stan.cehRab << endl << "Эффективность: " << stan.eff << endl;
 }
+
+void savePipe(Pipe& truba)
+{
+    ofstream fout;
+    fout.open("OutTruba.txt", ios::out);
+    if (fout.is_open()) {
+        fout << "Труба" << endl << "Id: " << truba.id << endl << "Диаметр: " << truba.d << "мм" << endl << "Длина: " << truba.l << "км" << endl << "В ремонте: ";
+        if (truba.rem) { fout << "да"; }
+        else { fout << "нет"; }
+        fout << endl;
+        fout.close();
+    }
+    
+}
+void saveStantia(Stantia& stan)
+{
+    ofstream fout;
+    fout.open("OutStantia.txt", ios::out);
+    if (fout.is_open()) {
+        fout << "Станция" << endl << "Id: " << stan.id << endl << "Название: " << stan.name << endl << "Кол-во цехов " << stan.ceh << endl << "Цехов в работе: " << stan.cehRab << endl << "Эффективность: " << stan.eff << endl;
+        fout.close();
+    }
+    
+}
+
 int main()
 {
     setlocale(LC_ALL, "Russian");
@@ -67,7 +138,7 @@ int main()
     cout << "Команда: ";
     cin >> com;
     
-    while (com != 0) {
+    while (1) {
        
         switch (com)
         {
@@ -87,7 +158,7 @@ int main()
             if (trubaExist) {
                 coutPipe(truba);
             }
-            else if (stanExist) {
+             if (stanExist) {
                 coutStantia(stan);
             }
             else {
@@ -111,7 +182,10 @@ int main()
             }
             else {
                 oldCehRab = stan.cehRab;
-                stan.cehRab = stan.cehRab-1;
+                if (stan.cehRab >= 1) {
+                    stan.cehRab = stan.cehRab - 1;
+                }
+                else { stan.cehRab = stan.cehRab; };
                 stan.eff = stan.cehRab*100/oldCehRab;
                 coutStantia(stan);
             }
@@ -121,28 +195,54 @@ int main()
             return 0;
             break;
         }
+        
+        case 6: {
+            if (trubaExist) {
+                savePipe(truba);
+            }
+            if (stanExist) {
+                saveStantia(stan);
+            }
+            else {
+                cout << "Ничего не создано" << endl;
+            }
+            cout << "Сохранено!";
+            break;
+        }
+        case 7: {
+            truba = loadPipe();
+            coutPipe(truba);
+            trubaExist = true;
+            stan = loadStantia();
+            coutStantia(stan);
+            stanExist = true;
+            break;
+        }
+        /*if (cin.fail()) {
+            cout << "Вводите цифру.\n";
+            break;
+        }*/
         default: {
             cout << "Нет такой команды.\n";
             break;
         }
+        
         }
+        
         cout << "Команда: ";
         cin >> com;
-        if (!cin) {
-            cin.clear();
-            cin.ignore(100);
-            cout << "Вводите номер команды.";
-        }
     }
 }
     //+цехи по вводу
     //в название водятся несколько слов
-    //если в название вводить цифры, тебя поправляют (и наоборот)
     //+переменную а убрать, сделать кейсы
     //+нецелые числа чтобы вводились
     //+если число 0, ритурн брейк, если другие, "нет такой команды"
-    //некорректный ввод чисел 
-    //чтение из файла, ввод в файл
+    //+некорректный ввод чисел 
+    //+чтение из файла, ввод в файл
+    //проверка ввода имени
+    // меню и редактирование - операции
+// проверка ввода команды
    
 
 
